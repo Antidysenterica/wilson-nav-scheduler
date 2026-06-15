00742b1464de10ad5f0b16e76587cdccf1b28c70
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-06-15 20:50:50 +0800

    Add Navbar component with navigation links

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>NAVI</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Campus Map</li>
+        <li>Appointments</li>
+        <li>Login</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
