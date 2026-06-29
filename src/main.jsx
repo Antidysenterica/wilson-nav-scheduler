7d4ed29ecbe2278d2f276727581520f91cd942f6
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-06-29 22:36:08 +0800

    Import Login.css in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/Login";
+import "./styles/Login.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
