1b5d29c23b377bdac585fb53f9d5a7a9e640030b
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-06-29 22:56:37 +0800

    Rename import from App to Login in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/Login";
+import Login from "./pages/Login";
 import "./styles/Login.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
