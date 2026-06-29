eed3c0110abf9625ea8a03af3fc9b3a4ffd431a3
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-06-29 23:10:49 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Login from "./pages/Login";
-import "./styles/Login.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
