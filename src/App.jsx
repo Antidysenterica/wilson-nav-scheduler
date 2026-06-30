bb5d3e65fc52ff67d0eda256a5560f36ebd577b2
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-06-29 21:33:00 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+// Fix this later on
 import Navbar from "./components/Navbar";
+import Map from "./pages/Home";
+import Login from "./pages/Home";
+import Appointment from "./pages/Home";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* Change Login to Landing Page later on (this is the one you will see if you want to log in or just want to view the map*/}
+        <Route path="/" element={<Login />} />\
+        
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Appointment" element={<Appointment />} />
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
