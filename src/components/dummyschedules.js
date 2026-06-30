610db0b00e12376c084306fade356c8570ec1847
Milly-coder17 <epanganiban@gbox.adnu.edu.ph>
2026-07-01 00:08:40 +0800

    Add files via upload

---
+++ src/components/dummyschedules.js
@@ -0,0 +1,120 @@
+export const availableSchedules = {
+    "2026-06-18": [
+        {
+            startTime: "9:00 AM",
+            endTime: "9:20 AM",
+            status: "Available"
+        },
+        {
+            startTime: "9:20 AM",
+            endTime: "9:40 AM",
+            status: "Pending"
+        },
+        {
+            startTime: "9:40 AM",
+            endTime: "10:00 AM",
+            status: "Available"
+        },
+        {
+            startTime: "10:00 AM",
+            endTime: "10:20 AM",
+            status: "Pending"
+        },
+        {
+            startTime: "10:20 AM",
+            endTime: "10:40 AM",
+            status: "Available"
+        },
+        {
+            startTime: "10:40 AM",
+            endTime: "11:00 AM",
+            status: "Available"
+        },
+        {
+            startTime: "11:00 AM",
+            endTime: "11:20 AM",
+            status: "Pending"
+        },
+        {
+            startTime: "11:20 AM",
+            endTime: "11:40 AM",
+            status: "Available"
+        },
+        {
+            startTime: "11:40 AM",
+            endTime: "12:00 PM",
+            status: "Pending"
+        },
+        {
+            startTime: "12:00 PM",
+            endTime: "12:20 PM",
+            status: "Available"
+        },
+    ],
+
+    "2026-06-19": [
+        {
+            startTime: "1:00 PM",
+            endTime: "1:20 PM",
+            status: "Available"
+        },
+        {
+            startTime: "1:20 PM",
+            endTime: "1:40 PM",
+            status: "Pending"
+        }
+    ],
+
+    "2026-06-20": [
+        {
+            startTime: "9:00 AM",
+            endTime: "9:20 AM",
+            status: "Available"
+        },
+        {
+            startTime: "9:20 AM",
+            endTime: "9:40 AM",
+            status: "Pending"
+        },
+        {
+            startTime: "9:40 AM",
+            endTime: "10:00 AM",
+            status: "Available"
+        },
+        {
+            startTime: "10:00 AM",
+            endTime: "10:20 AM",
+            status: "Available"
+        },
+        {
+            startTime: "10:20 AM",
+            endTime: "10:40 AM",
+            status: "Pending"
+        },
+        {
+            startTime: "10:40 AM",
+            endTime: "11:00 AM",
+            status: "Available"
+        },
+        {
+            startTime: "11:00 AM",
+            endTime: "11:20 AM",
+            status: "Pending"
+        },
+        {
+            startTime: "11:20 AM",
+            endTime: "11:40 AM",
+            status: "Available"
+        },
+        {
+            startTime: "11:40 AM",
+            endTime: "12:00 PM",
+            status: "Pending"
+        },
+        {
+            startTime: "12:00 PM",
+            endTime: "12:20 PM",
+            status: "Available"
+        },
+    ],
+};
