3dc26dcc79e14440f7425e949598ec001613af19
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-06-30 20:24:16 +0800

    Create TimeSlotEdit component for editing time slots

---
+++ src/pages/TimeSlotEdit.jsx
@@ -0,0 +1,10 @@
+function Map() {
+  return (
+    <div>
+      <h1>Edit Available Time Slot</h1>
+      <img src="/campus-map.png" alt="Map" width="600" />
+    </div>
+  );
+}
+
+export default Map;
