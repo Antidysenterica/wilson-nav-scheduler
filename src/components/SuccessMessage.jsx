4ebb8736a5cdfa6c1ce96301096996c9d6e6342d
Milly-coder17 <epanganiban@gbox.adnu.edu.ph>
2026-07-01 00:41:59 +0800

    Add files via upload

---
+++ src/components/SuccessMessage.jsx
@@ -0,0 +1,24 @@
+function SuccessMessage() {
+    return (
+        <div
+            style={{
+                width: "730px",
+                height: "16px",
+                fontFamily: "Intel, sans-serif",
+                fontSize: "14px",
+                textAlign: "center",
+                alignItems: "center",
+                backgroundColor: "hsl(48, 100%, 88%)",
+                color: "hsl(183, 50%, 29%)",
+                margin: "0 auto",
+                padding: "12px",
+                borderRadius: "6px",
+                fontWeight: "bold"
+            }}
+        >
+            Requested Successfully
+        </div>
+    );
+}
+
+export default SuccessMessage;
