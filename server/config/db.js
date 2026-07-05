623527b548e8742c754c4472211329dfe83732cd
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-07-05 17:52:11 +0800

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "root",
   database: process.env.DB_NAME || "wilson_db",
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
+  port: process.env.DB_PORT || 3306,
 });
 
 db.connect((err) => {
