766e0bd562faaf30c9e286664ceb02449b1c4fdd
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-07-06 21:59:59 +0800

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   ssl: {
-    ca: fs.readFileSync("../ca.pem")
+    ca: fs.readFileSync("./ca.pem")
   }
 });
 
