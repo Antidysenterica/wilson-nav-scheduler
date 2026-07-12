0e9f52039b4753dae6aa715461b9eb0d89da45ce
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-07-12 18:30:06 +0800

    Wrap App component with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
