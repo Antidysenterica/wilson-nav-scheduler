17564ab0cfbc83421886c0c48399e5a8ac7c61a6
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-07-12 18:52:13 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import ManageAppointment from "./pages/ManageAppointment";
 import Map from "./pages/Map";
 import Profile from "./pages/Profile";
 import TimeSlotEdit from "./pages/TimeSlotEdit";
-// import NotFound from "./pages/NotFound";
+import NotFound from "./pages/NotFound";
 
 // Components
 import Navbar from "./components/Navbar";
