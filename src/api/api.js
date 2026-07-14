ed165e124ed2734c8fc4d7ea3bebfff3891ebb46
Adrian Justin R. Pana <ajpana@gbox.adnu.edu.ph>
2026-07-14 23:48:40 +0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL:"http://localhost:5000/api/buildings"
+    baseURL:"https://wilson-nav-backend.onrender.com/api/buildings"
 
 });
 
